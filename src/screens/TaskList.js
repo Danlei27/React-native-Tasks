10958e760e78f4f959b1064e9bf9bdebaed15082
danlei santos <danleisantos@hotmails.com>
2020-11-28 16:56:19 -0300

    componente task

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -4,7 +4,7 @@ import todayImage from '../../assets/imgs/today.jpg'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 import commonStyles from '../commonStyles'
-
+import Task from '../components/Task'
 export default class TaskList extends Component {
     render(){
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
@@ -18,9 +18,9 @@ export default class TaskList extends Component {
                         </View>
                     </ImageBackground>
                 <View style={styles.taskList}>
-                    <Text>TaskList</Text>
+                <Task desc="Comprar Livro" estimateAt={new Date()} doneAt={new Date()}/>
+                <Task desc="Ler Livro"     estimateAt={new Date()} doneAt={null}/>    
                 </View>
-
             </View>
 
         )
