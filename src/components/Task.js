6e2245a0a02a34b7f2d4840c79afa5fe172d57a9
danlei santos <danleisantos@hotmails.com>
2020-11-28 19:39:56 -0300

    Estilizando  task, fomatando texto de desc, date

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,17 +2,26 @@ import React from 'react'
 import {View, Text, StyleSheet } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
+import moment from 'moment'
+import 'moment/locale/pt-br'
 import commonStyles from '../commonStyles'
 
 export default props => {
+    const doneOrNotStyle = props.doneAt != null ?
+        { textDecorationLine: 'line-through'} : {}
+
+    const date = props.doneAt ? props.doneAt : props.estimateAt
+    const formattedDate = moment(date).locale('pt-br')
+        .format('ddd, D [de] MMMM')
+
     return(
         <View style={styles.container}>
             <View style={styles.checkContainer}>
                 {getCheckView(props.doneAt)}
             </View>
             <View>
-                <Text>{props.desc}</Text>
-                <Text>{props.estimateAt + ""}</Text>
+                <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
+                <Text style={styles.date}>{formattedDate}</Text>
             </View>
             
         </View>
@@ -62,5 +71,14 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
         
+    },desc:{
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.mainText,
+        fontSize: 15
+    },
+    date: { 
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.subText,
+        fontSize: 12
     }
 })
