e1b5051fd229e6eb51ba731781546e722e4c6ae7
danlei santos <danleisantos@hotmails.com>
2020-11-28 09:54:35 -0300

    componente taskList

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground } from 'react-native'
+import todayImage from '../../assets/imgs/today.jpg'
 
 export default class TaskList extends Component {
     render(){
         return (
-            <View>
-                <Text>TaskList</Text>
+            <View style={styles.container} >
+
+                <View style={styles.image}>
+                    <ImageBackground source={todayImage} style={styles.image}> 
+                    </ImageBackground>
+                </View>
+
+                <View style={styles.taskList}>
+                    <Text>TaskList</Text>
+                </View>
+
             </View>
+
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1
+    },
+    image:{
+        flex:3
+    },
+    taskList:{
+        flex:7
+    }
+})
