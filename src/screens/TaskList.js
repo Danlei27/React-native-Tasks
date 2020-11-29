f4f1c4d94e63db7f80a93d3753318a7869fb2743
danlei santos <danleisantos@hotmails.com>
2020-11-28 22:50:16 -0300

    Estilizando icon, eyeque filtrar tarefas, pending ou done

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, ImageBackground, FlatList  } from 'react-native'
+import { View, Text, StyleSheet, ImageBackground, FlatList, TouchableOpacity, Plataform, Platform } from 'react-native'
 import todayImage from '../../assets/imgs/today.jpg'
+import Icon from 'react-native-vector-icons/FontAwesome'
 import moment from 'moment'
 import 'moment/locale/pt-br'
 import commonStyles from '../commonStyles'
 import Task from '../components/Task'
 export default class TaskList extends Component {
     state = {
+        showDoneTasks: true,
         tasks: [
             {
             id: Math.random(),
@@ -107,6 +109,11 @@ export default class TaskList extends Component {
         ]
     }
     
+
+    toggleFilter = () => {
+        this.setState({ showDoneTasks: !this.state.showDoneTasks })
+    }
+    
     toggleTask = taskId => {
         const tasks = [...this.state.tasks]
         tasks.forEach(task =>{
@@ -124,6 +131,12 @@ export default class TaskList extends Component {
             <View style={styles.container} >
                     <ImageBackground source={todayImage} 
                     style={styles.image}> 
+                        <View style={styles.iconBar}>
+                            <TouchableOpacity onPress={this.toggleFilter}>
+                                <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                                size={20} color={commonStyles.colors.secondary}/>
+                            </TouchableOpacity>
+                        </View>
                         <View style={styles.titleBar}>
                                 <Text style={styles.title}>Hoje</Text>
                                 <Text style={styles.subtitle}>{today}</Text>
@@ -168,5 +181,11 @@ const styles = StyleSheet.create({
         fontSize: 20,
         marginLeft: 20,
         marginBottom: 20
+    },
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'flex-end',
+        marginTop: Platform.OS === 'ios' ? 40 : 10
     }    
 })
