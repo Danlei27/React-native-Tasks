98c17f5bb2c716d38947a3b5ab26d9a646a8c7f1
danlei santos <danleisantos@hotmails.com>
2020-11-29 18:09:24 -0300

    finalizando botão de ocultar tarefas, concluida

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -9,6 +9,7 @@ import Task from '../components/Task'
 export default class TaskList extends Component {
     state = {
         showDoneTasks: true,
+        visibleTasks:[],
         tasks: [
             {
             id: Math.random(),
@@ -21,97 +22,28 @@ export default class TaskList extends Component {
             desc: 'Ler livro de React Native',
             estimateAt: new Date(),
             doneAt: null,
-        },
-        {
-            id: Math.random(),
-            desc: 'Comprar Livro de React Native',
-            estimateAt: new Date(),
-            doneAt: new Date(),
-        },
-        {
-            id: Math.random(),
-            desc: 'Ler livro de React Native',
-            estimateAt: new Date(),
-            doneAt: null,
-        },
-        {
-            id: Math.random(),
-            desc: 'Comprar Livro de React Native',
-            estimateAt: new Date(),
-            doneAt: new Date(),
-        },
-        {
-            id: Math.random(),
-            desc: 'Ler livro de React Native',
-            estimateAt: new Date(),
-            doneAt: null,
-        },
-        {
-            id: Math.random(),
-            desc: 'Comprar Livro de React Native',
-            estimateAt: new Date(),
-            doneAt: new Date(),
-        },
-        {
-            id: Math.random(),
-            desc: 'Ler livro de React Native',
-            estimateAt: new Date(),
-            doneAt: null,
-        },
-        {
-            id: Math.random(),
-            desc: 'Comprar Livro de React Native',
-            estimateAt: new Date(),
-            doneAt: new Date(),
-        },
-        {
-            id: Math.random(),
-            desc: 'Ler livro de React Native',
-            estimateAt: new Date(),
-            doneAt: null,
-        },
-        {
-            id: Math.random(),
-            desc: 'Comprar Livro de React Native',
-            estimateAt: new Date(),
-            doneAt: new Date(),
-        },
-        {
-            id: Math.random(),
-            desc: 'Ler livro de React Native',
-            estimateAt: new Date(),
-            doneAt: null,
-        },
-        {
-            id: Math.random(),
-            desc: 'Comprar Livro de React Native',
-            estimateAt: new Date(),
-            doneAt: new Date(),
-        },
-        {
-            id: Math.random(),
-            desc: 'Ler livro de React Native',
-            estimateAt: new Date(),
-            doneAt: null,
-        },
-        {
-            id: Math.random(),
-            desc: 'Comprar Livro de React Native',
-            estimateAt: new Date(),
-            doneAt: new Date(),
-        },
-        {
-            id: Math.random(),
-            desc: 'Ler livro de React Native',
-            estimateAt: new Date(),
-            doneAt: null,
-        },
+        }
         ]
     }
     
+    componentDidMount = () => { // método de ciclo de vida
+        this.filterTasks()
+    }
 
     toggleFilter = () => {
-        this.setState({ showDoneTasks: !this.state.showDoneTasks })
+        this.setState({ showDoneTasks: !this.state.showDoneTasks}, this.filterTasks)
+    }
+    
+    filterTasks = () => {
+        let visibleTasks = null
+        if(this.state.showDoneTasks) {
+            visibleTasks = [...this.state.tasks]
+        }else {
+            const pending = task => task.doneAt === null
+            visibleTasks = this.state.tasks.filter(pending)
+        }
+
+        this.setState({ visibleTasks })
     }
     
     toggleTask = taskId => {
@@ -122,7 +54,7 @@ export default class TaskList extends Component {
             }
         })
 
-        this.setState({ tasks })
+        this.setState({ tasks }, this.filterTasks)
     }
 
     render(){
@@ -143,7 +75,7 @@ export default class TaskList extends Component {
                         </View>
                     </ImageBackground>
                 <View style={styles.taskList}>
-                    <FlatList data={this.state.tasks}
+                    <FlatList data={this.state.visibleTasks}
                         keyExtractor={item => `${item.id}`}
                         renderItem={({item}) => <Task {...item}
                         toggleTask={this.toggleTask}/>}/>
