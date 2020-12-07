7a039420a2a2342cd2385cde946a5477cb6ff1ba
danlei santos <danleisantos@hotmails.com>
2020-12-07 19:58:48 -0300

    Validando Formulário

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -67,6 +67,16 @@ export default class Auth extends Component{
     }
 
     render(){
+        const validations = []
+        validations.push(this.state.email && this.state.email.includes('@'))
+        validations.push(this.state.password && this.state.password.length >= 6)
+
+        if(this.state.stagNew){
+            validations.push(this.state.name && this.state.name.trim().length >= 3)
+            validations.push(this.state.password === this.state.confirmPassword)
+        }
+
+        const validForm = validations.reduce((t, a) => t && a)
         return (
             <ImageBackground source={backgroundImage}
                 style={styles.background}>
@@ -91,8 +101,9 @@ export default class Auth extends Component{
                         value={this.state.confirmPassword} secureTextEntry={true}
                         style={styles.input} onChangeText={confirmPassword => this.setState({ confirmPassword })}/>
                     }   
-                    <TouchableOpacity onPress={this.signinOrSignup}>
-                        <View style={styles.button}>
+                    <TouchableOpacity onPress={this.signinOrSignup}
+                        disabled={!validForm}>
+                        <View style={[styles.button, validForm ? {} : {backgroundColor: '#AAA'}]}>
                             <Text style={styles.buttonText}>{this.state.stageNew ? 'Registrar': 'Entrar'}</Text>
                         </View>
                     </TouchableOpacity>
