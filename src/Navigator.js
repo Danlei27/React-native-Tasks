6a12767b496741f654f19e3b77e80e7f29ed37ca
danlei santos <danleisantos@hotmails.com>
2020-12-08 09:18:25 -0300

    Drawer Navigator #04

--- src/Navigator.js
+++ src/Navigator.js
@@ -3,9 +3,27 @@ import { createSwitchNavigator } from '@react-navigation/compat'
 // import {  createAppContainer , createSwitchNavigator } from 'react-navigation'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { NavigationContainer } from '@react-navigation/native'
+import Menu from './screens/Menu'
+import commonStyles from './commonStyles'
 
 import Auth from './screens/Auth'
 import TaskList from './screens/TaskList'
+
+const menuConfig = {
+    initialRouteName: 'Today',
+    contentComponent: Menu,
+    contentOptions: {
+        labelStyle:{
+            fontFamily: commonStyles.fontFamily,
+            fontWeight: 'normal',
+            fontSize: 20
+        },
+        activeLabelStyle: {
+            color: '#080',
+            fontWeight: 'normal',
+        }
+    }
+}
 
 const menuRoutes = {
     Today: {
@@ -38,7 +56,7 @@ const menuRoutes = {
     },
 },
 
-const menuNavigator = createDrawerNavigator(menuRoutes)
+const menuNavigator = createDrawerNavigator(menuRoutes, menuConfig)
 
 const mainRoutes = {
     Auth: {
